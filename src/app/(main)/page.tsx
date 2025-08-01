1b9d28b09e5017ed9c0171153d760b2f1e4dc15c
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-08-01 10:11:43 +0330

    create aside for basket stock

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -21,6 +21,8 @@ export default function ShoppingCart() {
           ))}
         </div>  
       </section>
+
+      d
     </>
   )
 }
--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import './header.css'
 export default function Header() {
+
+    const [basket, setBasket] = React.useState(false);
+    const showBasket = () => {
+        setBasket(true);
+    }
+
   return (
-    <header className='bg-white h-16 shadow flex items-center container'>
+    <header className='bg-white h-18 shadow flex items-center container'>
         <nav className='h-full flex items-center justify-between w-full py-2'>
             <div className='h-full flex items-center'>
             <a className='border-r-2 border-gray-400 px-3 h-full flex-row-center text-2xl'>Boxing club</a>
@@ -19,7 +25,7 @@ export default function Header() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
             </button>
-            <button className='relative bg-[#EDEFF1] border border-gray-300 rounded-full w-10 h-10 flex-row-center'>
+            <button onClick={showBasket} className='relative bg-[#EDEFF1] border border-gray-300 rounded-full w-10 h-10 flex-row-center'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                 </svg>
@@ -27,6 +33,17 @@ export default function Header() {
             </button>
             </div>
         </nav>
+
+        <div className={`${basket ? 'translate-x-0' : 'translate-x-full'} basket bg-white absolute top-0 bottom-0 right-0 w-[303px] shadow px-9 py-5`}>
+            <div className='flex justify-between items-center'>
+                <h2 className='text-lg font-semibold'>Boxing Club</h2>
+                <button onClick={() => setBasket(false)} className='border rounded-full p-1.5 hover:bg-black hover:text-white transition-colors ease-in duration-75'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+        </div>
     </header>
   )
 }
