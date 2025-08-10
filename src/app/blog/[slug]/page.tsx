575adfcb53548d729460cd336b93494abd6f04a3
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-08-10 23:47:58 +0330

    put go back btn on blog page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import blogs from '../../(main)/blogs'
 import Image from 'next/image';
-
+import Link from 'next/link';
 interface BlogPageParams {
   slug: string;
 }
@@ -14,11 +14,11 @@ export default function BlogPage({ params }: { params: BlogPageParams }) {
   }
 
   return (
-    <div  className='flex-row-center my-11 container'>
+    <div  className='flex-col-center my-11 container'>
       <div className='grid grid-cols-1 md:grid-cols-6 bg-white gap-4 p-2 shadow-[var(--cartShadow)]'>
 
-        <div className='md:col-span-2 w-full bg-amber-500 flex-row-center'>
-          <Image src={blog.img} width={444} height={444} alt="" className='w-full h-auto object-cover' />  
+        <div className='md:col-span-2 w-full flex-row-center'>
+          <Image src={blog.img} width={444} height={444} alt={blog.img} className='w-full h-auto object-cover' />  
         </div>
 
         <div className='md:col-span-4 h-full flex flex-col justify-between p-3 gap-11 md:gap-0'>
@@ -31,15 +31,23 @@ export default function BlogPage({ params }: { params: BlogPageParams }) {
 
           <div>
             <div className="flex gap-3 items-center">
-              <Image src={blog.authorImg} width={44} height={44} alt=""/>  
+              <Image src={blog.authorImg} width={44} height={44} alt={blog.authorName} />  
               <div className="text-sm">
-                <h3 className="font-semibold">Phoenix Baker</h3>
-                <span className="text-[#667085]">19 Jan 2022</span>
+                <h3 className="font-semibold">{blog.authorName}</h3>
+                <span className="text-[#667085]">{blog.date}</span>
               </div>
             </div>
           </div>
 
         </div>
+
+      </div>
+      <div className="py-11 container flex-row-center">
+        <Link href="/">
+          <button className="flex-row-center gap-2 text-[#6941C6] bg-[#6941C6]/8 px-7 py-2 rounded-lg hover:cursor-pointer">
+            Go back
+          </button>
+        </Link>
       </div>
     </div>
   )
